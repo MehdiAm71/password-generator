6fd54842e5b47ad8aa1cbfebbfd761d2a765b85a
Mehdi <mehdistd33@gmail.com>
2026-09-03 14:02:04 +0200

    add new word in 'DEFAULT_WORDLIST'

--- password_generator.py
+++ password_generator.py
@@ -29,6 +29,8 @@ DEFAULT_WORDLIST = [
     "coral", "delta", "ember", "fable", "grove", "honey", "input", "joker",
     "kayak", "lunar", "mirth", "nexus", "orbit", "pearl", "quiet", "ridge",
     "sable", "trend", "unity", "vapor", "willow", "xerox", "yacht", "zonal",
+    "mouse", "floor", "leo", "fly", "back", "hero", "really", "fake",
+
 ]
 
 LEET_MAP = {"a": "4", "e": "3", "i": "1", "o": "0", "s": "5", "t": "7"}
